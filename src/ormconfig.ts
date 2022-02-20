d3f76ca026a59fecc800af77f086dd148bb8aee1
Алексей Польчиков <keksley@gmail.ru>
2022-02-20 19:18:03 +0500

    Сконфигурирована бд

---
+++ src/ormconfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'messenger-admin',
+  password: '123',
+  database: 'messenger',
+  synchronize: false,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export default config;
